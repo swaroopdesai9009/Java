4be91b4e58df43ce718ee66c80ad0c1774fde658
swaroopdesai9009 <swaroopdesai93@gmail.com>
2026-03-16 17:58:30 +0530

    Added comment

--- Generic.java
+++ Generic.java
@@ -1,3 +1,9 @@
+//         Generic gc = new Generic(10,20);
+//         System.out.println(gc.getName());
+//         Generic gc1 = new Generic("swaroop",1000000);
+//         System.out.println(gc1.getName());
+//         }
+
 public class Generic <T>{
     T name;
     T salary;
